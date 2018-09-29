78683b7d2b81760228d583248893d4f21d82cb22
Eugene <levchuk.eugene@gmail.com>
2018-09-29 21:02:24 +0300

    zeros-Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let numNulls=0
+  while(number!==0){
+    numNulls+=Math.floor(number/5)
+    number=Math.floor(number/5)
+  }
+  return numNulls;
 }
